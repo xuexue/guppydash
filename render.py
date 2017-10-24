fd05746eb1d761d31643ce3ad430827005558a63
Lisa Zhang <lisa@polychart.com>
2017-10-24 16:53:43 -0400

    Identify interactive jobs; mark long-running ones

--- render.py
+++ render.py
@@ -36,15 +36,43 @@ class GPU(object):
 
 class Job(object):
     """Representation of a job being run"""
-    def __init__(self, user, node, ngpu, ncpu, time):
+    def __init__(self, user, node, ngpu, ncpu, time, int):
         self.user = user # user who launched job
         self.node = node # GPU object
         self.ngpu = ngpu # number of gpu used
         self.ncpu = ncpu # number of cpu used
         self.time = time # string representation of time taken
+        self.int = int # bool interactive job
     def __str__(self):
         return "Job by %s on %s using %d gpu %d cpu" % (
                 self.user.name, self.node.name, self.ngpu, self.ncpu)
+    def tooltip(self):
+        if self.is_long_interactive_job():
+            return "This is a long-running interactive job!"
+        return self.user.tooltip()
+    def colour_class(self):
+        """Return the css class (controlling colour) for the job"""
+        if self.is_long_interactive_job():
+            return "alert-danger"
+        if self.user.is_overusing():
+            return "alert-warning"
+        return "alert-info"
+    def is_long_interactive_job(self):
+        """Returns true iff job is interactive, and has been running for
+        at least 2 hours"""
+        if not self.int:
+            return False
+        # if self.time is at least 8 characters long, the format is at least
+        # "<optionally more>HH:MM:DD", so has been running for > 10 hours
+        if len(self.time) > 7:
+            return True
+        # if self.time is less than 7 characters long, has not been running
+        # for more than an hour
+        if len(self.time) < 7:
+            return False
+        # now, self.time is exactly 7 charslong, check if first char is a "1"
+        # if it is not a '1' it must be >= 2 hours
+        return self.time[0] != '1'
 
 class User(object):
     def __init__(self, name):
@@ -77,12 +105,28 @@ def read_gpu_avail():
                          up=(status == 'up')) 
     return gpus
 
-def read_jobs(gpus):
+def read_pty_bash_jobs():
+    """
+    Get the username and time submitted of --pty bash jobs.
+    Unfortunately there is no way of getting this information from slurm
+    """
+    jobs = set()
+    for line in query_cluster('ps -eo user,lstart,cmd | grep "pty bash" | grep "srun" | grep -v "grep"'):
+        line = [x for x in line.strip().split(" ") if x]
+        user = line[0]
+        lstart = datetime.datetime.strptime(' '.join(line[1:6]),
+                                            "%a %b %d %H:%M:%S %Y")
+        jobs.add((user, lstart))
+    return jobs
+
+def read_jobs(gpus, interactive_jobs):
     # read
     users = {}
     jobs = []
-    for line in query_cluster('squeue -o "%u\t%b\t%C\t%N\t%M\t%F\t%o" -h;'):
-        username, gpu, cpu, gpuname, time, jobid, cmd = line.strip().split('\t')
+    for line in query_cluster('squeue -o "%u\t%b\t%C\t%N\t%M\t%F\t%V\t%j" -h;'):
+        username, gpu, cpu, gpuname, time, jobid, lstart, cmd = line.strip().split('\t')
+        # convert time
+        lstart = datetime.datetime.strptime(lstart, "%Y-%m-%dT%H:%M:%S")
         # sometimes the gpu list is incomplete, put in a placeholder for now
         if gpuname not in gpus:
             gpus[gpuname] = GPU(gpuname, up=False)
@@ -93,7 +137,8 @@ def read_jobs(gpus):
                   gpus[gpuname],
                   ngpu=(0 if gpu == "(null)" else int(gpu.split(':')[1])),
                   ncpu=int(cpu),
-                  time=time)
+                  time=time,
+                  int=(cmd == "bash") and (username, lstart) in interactive_jobs)
         # append to jobs
         jobs.append(job)
         gpus[gpuname].add_job(job)
@@ -117,6 +162,8 @@ def render(gpus, jobs, users):
     open('index.html', 'w').write(output)
 
 if __name__ == '__main__':
+    interactive_jobs = read_pty_bash_jobs()
+
     gpus = read_gpu_avail() # from running Cluster Usage</h1>
-    jobs, users = read_jobs(gpus)
+    jobs, users = read_jobs(gpus, interactive_jobs)
     render(gpus, jobs, users)
