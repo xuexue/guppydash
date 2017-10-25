1a1610e4c8c64c490dd79ed0e7a64efd5c9e0cc1
Lisa Zhang <lisa@polychart.com>
2017-10-25 16:12:16 -0400

    Add hack to display WAITING jobs

--- render.py
+++ render.py
@@ -127,6 +127,8 @@ def read_jobs(gpus, interactive_jobs):
         username, gpu, cpu, gpuname, time, jobid, lstart, cmd = line.strip().split('\t')
         # convert time
         lstart = datetime.datetime.strptime(lstart, "%Y-%m-%dT%H:%M:%S")
+        if not gpuname:
+            gpuname = "WAITING"
         # sometimes the gpu list is incomplete, put in a placeholder for now
         if gpuname not in gpus:
             gpus[gpuname] = GPU(gpuname, up=False)
